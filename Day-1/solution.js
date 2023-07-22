b054a139e98039c5b6f33ffa85f345cb34148d31
manish-are <manisharebsnl@gmail.com>
2023-07-22 16:53:38 +0530

    part-2 solution added

--- Day-1/solution.js
+++ Day-1/solution.js
@@ -17,26 +17,66 @@ fs.readFile(filePath,'utf8',(err,data) => {
                     'E': {'L': ['N', [0, 1]], 'R': ['S', [0,-1]] },
                     'W': {'L': ['S', [0, -1]], 'R': ['N', [0, 1]] } }; 
 
-    let startingPosition = [0, 0];
+
+
+    let visited = new Set();
+    function part1(){
+        let startingPosition = [0, 0];
     
+        let currentDirection = 'N';
+        let currentPosition = [0, 0];
+        for (let instr of instrs){
+
+            let [movement, blocks] = [instr[0], parseInt(instr.slice(1))];
     
-
-    let currentDirection = 'N';
-    let currentPosition = [0, 0];
-
-    for (let instr of instrs){
-
-        let [movement, blocks] = [instr[0], parseInt(instr.slice(1))];
-
-        let nextDirection = rules[currentDirection][movement][0];
-        let nextDirectionCoordinates = rules[currentDirection][movement][1];
-        
-        //change currentDirection and currentPosition according to rules
-        currentPosition[0] = currentPosition[0] + blocks * nextDirectionCoordinates[0];
-        currentPosition[1] = currentPosition[1] + blocks * nextDirectionCoordinates[1];
-        currentDirection = nextDirection;
+            let nextDirection = rules[currentDirection][movement][0];
+            let nextDirectionCoordinates = rules[currentDirection][movement][1];
+            
+            //change currentDirection and currentPosition according to rules
+            currentPosition[0] = currentPosition[0] + blocks * nextDirectionCoordinates[0];
+            currentPosition[1] = currentPosition[1] + blocks * nextDirectionCoordinates[1];
+            currentDirection = nextDirection;
         }
-        console.log(Math.abs(currentPosition[0] - startingPosition[0]) + Math.abs(currentPosition[1] - startingPosition[1]));
+    
+        return Math.abs(currentPosition[0] - startingPosition[0]) + Math.abs(currentPosition[1] - startingPosition[1]);
+
+    }
+    
+    function part2(){
+        let startingPosition = [0, 0];
+        let currentDirection = 'N';
+        let currentPosition = [0, 0];
+        for (let instr of instrs){
+
+            let [movement, blocks] = [instr[0], parseInt(instr.slice(1))];
+            
+    
+            let nextDirection = rules[currentDirection][movement][0];
+            let nextDirectionCoordinates = rules[currentDirection][movement][1];
+            currentDirection = nextDirection;
+            while(blocks > 0){
+                currentPosition[0] = currentPosition[0] + nextDirectionCoordinates[0];
+                currentPosition[1] = currentPosition[1] + nextDirectionCoordinates[1];
+                if(visited.has(`${currentPosition[0]}-${currentPosition[1]}`)){
+                
+                    return Math.abs(currentPosition[0] - startingPosition[0]) + Math.abs(currentPosition[1] - startingPosition[1]);
+    
+                }else{
+                    
+                    visited.add(`${currentPosition[0]}-${currentPosition[1]}`);
+                    
+                }
+                blocks = blocks - 1;
+            }  
+        }
+        return "not found";
+
+    }
+    
+    console.log(`part1: ${part1()}`);
+    console.log(`part2: ${part2()}`);
+    
+        
   
     });
 
